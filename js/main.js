8e9399245972e52ffe8cc95913f2ae33050d12fa
Cesar Gomez <00.13.zkr@gmail.com>
2015-12-04 11:57:23 -0600

    !ecs6

--- js/main.js
+++ js/main.js
@@ -1,57 +1,75 @@
-"use strict";
-const apiKey = "mwteuqmdy4crfdvmswvzwkpf";
+var apiKey = "mwteuqmdy4crfdvmswvzwkpf";
 var Productos = (localStorage.Productos === undefined) ? [] : JSON.parse(localStorage.Productos);
 var Total = 0;
-$(()=>{
-	ProductInfo();
-	$("#scann").on("click", (e)=>{
-		let code = QRCode();		
-	});
+$(function (){
+    ProductInfo();
+    $("#scann").on("click", QRCode);
 });
-function ProductInfo(){
-	let rows = [];
-	$.Enumerable.From(Productos).ForEach(m=>{
-		rows.push($("<li>",{class:"collection-item", html:`<h3>${m.items[0].name}</h3><p>$${m.items[0].salePrice}</p><img src="${m.items[0].thumbnailImage}">`}));
-		Total += Number(m.salePrice);
-	});
-	$("#Productos").html(rows);
+
+function ProductInfo () {
+    var rows = [];
+    $.Enumerable.From(Productos).ForEach(m => {
+        rows.push($("<li>", {
+            class: "collection-item",
+            html: `<h3>${m.items[0].name}</h3><p>$${m.items[0].salePrice}</p><img src="${m.items[0].thumbnailImage}">`
+        }));
+        Total += Number(m.salePrice);
+    });
+    $("#Productos").html(rows);
 }
-function QRCode(){
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-        scanner.scan( function (result) {        
-            if(!result.cancelled){
-                call(result.text);
-            } 
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
-    }
-function call(number){
-	$.ajax({
-		url: `http://api.walmartlabs.com/v1/items?apiKey=${apiKey}&upc=${number}`,
-		type: 'GET',
-		contentType: "application/json; charset=utf-8",
-		dataType: 'jsonp',
-		data: {},
-		crossDomain: true,
-	})
-	.done(function(r) {
-		console.log("success", r);
-		if(!r.errors){
-			Productos.push(r);
-			localStorage.Productos = JSON.stringify(Productos);
-			ProductInfo();			
-		}else{
-			alert("ERROR", r.errors[0].message);
-		}
-	})
-	.fail(function(err) {
-		console.error("error", err);
-	});
-}  
+
+function QRCode () {
+    var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    scanner.scan(function(result) {
+        if (!result.cancelled) {
+            call(result.text);
+        }
+    }, function(error) {
+        console.log("Scanning failed: ", error);
+    });
+}
+
+function call (number) {
+    $.ajax({
+            url: "http://api.walmartlabs.com/v1/items?apiKey={0}&upc={1}".format(apiKey,number),
+            //url: "http://api.upcdatabase.org/json/6ee2e78a090274caaf946b79d836f1db/"+number,
+            type: 'POST',
+            dataType: 'jsonp',
+            data: {},
+            crossDomain: true,
+        })
+        .done(function(r) {
+            console.log("success", r);
+            if (!r.errors) {
+                //Productos.push(r);
+                //localStorage.Productos = JSON.stringify(Productos);
+                //ProductInfo();
+            } else {
+                alert("ERROR", r);
+            }
+        })
+        .fail(function(err) {
+            console.error("error", err);
+        });
+}
 //****************************************************************//
-$.fn.serializeObject = function()
-{
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
+}
+if (!String.prototype.trim) {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    };
+}
+$.fn.serializeObject = function() {
     var o = {};
     var a = this.serializeArray();
     $.each(a, function() {
